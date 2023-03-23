08f153c910f94769ed35a569084487df7a5a9791
Thanh Long <dtlong.work@gmail.com>
2023-03-24 00:21:54 +0700

    backend commic demo

--- src/home/dto/repository/home.repository.ts
+++ src/home/dto/repository/home.repository.ts
@@ -6,9 +6,8 @@ export class HomeRepository {
     constructor(private readonly imageService: ImageService) { }
     async loopObject(listObjects: any, createObjectDto: any, createObjects: any): Promise<void> {
         for (const object of listObjects) {
-            console.log(this.imageService);
             let image = await this.imageService.findImageById(object.image_id);
-            const createDataDto = new createObjectDto(object.chapter_intro, image.path)
+            const createDataDto = new createObjectDto(object.chapter_intro ?? object.title, image.path)
             createObjects.push(createDataDto);
         }
     }
