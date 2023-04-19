effc5ed59323a11c89dab318e6bbdb12f13e4ecf
huy2nf <tranxuanhuy3201@gmail.com>
2023-04-19 19:12:05 +0700

    add migration - mongoose

---
+++ migrations/1681903790641-add-age-to-publishers.ts
@@ -0,0 +1,36 @@
+'use strict'
+require('dotenv').config()
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const MONGO_URI = process.env.MONGO_URI
+
+module.exports.up = async function (next) {
+
+  const client = await MongoClient.connect(MONGO_URI, { useUnifiedTopology: true })
+  const db = client.db()
+  const collection = db.collection('publishers')
+  await collection.updateMany({}, { $set: { age: 0 } })
+  await client.close()
+  next()
+}
+
+module.exports.down = async function (next) {
+  const client = await MongoClient.connect(MONGO_URI, { useUnifiedTopology: true })
+  const db = client.db()
+  const collection = db.collection('publishers')
+  await collection.updateMany({}, { $unset: { age: "" } })
+  await client.close()
+  next()
+
+}
+
+//Tạo 
+// migrate create <name>
+// Chạy để thêm
+// migrate up
+// migrate up : migrates/<name>
+// Chạy để xóa
+// migrate down
+// migrate down : migrates/<name>
